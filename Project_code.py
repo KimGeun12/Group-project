29a9aa6a37767c4d380f3c265f66ddc406274769
[KimGeun12] <[sd8582a@gachon.ac.kr]>
2023-12-08 19:48:43 +0900

    final project code

--- Project_code.py
+++ Project_code.py
@@ -1,30 +1,33 @@
 import cv2
 import numpy as np
 
-# Load the original image
-image_path = 'C:\\Users\\User\\Desktop\image\\image.jpg'
+image_path = 'C:\\Users\\User\\Desktop\\image\\image.jpg'
 image = cv2.imread(image_path)
 
-# Check if the image is loaded properly
 if image is None:
     print("Error: 이미지를 불러오는 데 실패했습니다. 경로를 확인해 주세요.")
 else:
+    # Flip the image horizontally
+    flipped_image = cv2.flip(image, 1)
+
     # Convert to grayscale and apply edge detection
-    gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
+    gray = cv2.cvtColor(flipped_image, cv2.COLOR_BGR2GRAY)
     edges = cv2.Canny(gray, 100, 200)
 
-    # Dilate the edges to make them thicker
-    kernel = np.ones((5,5), np.uint8)  # Adjust kernel size for thickness
-    dilated_edges = cv2.dilate(edges, kernel, iterations=1)
+    # Find contours from the edges
+    contours, hierarchy = cv2.findContours(edges, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
 
-    # Create an image to draw thick colored contours
-    thick_contour_image = np.zeros_like(image)
+    # Create an image to draw thick contours
+    contour_image = np.zeros_like(flipped_image)
 
-    # Extract colors from the original image where thick edges are found
-    thick_contour_image[dilated_edges != 0] = image[dilated_edges != 0]
+    # Draw the contours on the image with a certain thickness
+    cv2.drawContours(contour_image, contours, -1, (255, 255, 255), thickness=3)
 
     # Save the result
-    thick_contour_image_path = 'C:\\Users\\User\\Desktop\\main\\image_thick_contours.jpg'
-    cv2.imwrite(thick_contour_image_path, thick_contour_image)
+    contour_image_path = 'C:\\Users\\User\\Desktop\\image\\contour_image.jpg'
+    success = cv2.imwrite(contour_image_path, contour_image)
 
-    print("색깔이 반영된 두꺼운 테두리가 그려진 이미지가 저장되었습니다:", thick_contour_image_path)
+    if success:
+        print("도형의 두꺼운 테두리가 그려진 이미지가 저장되었습니다:", contour_image_path)
+    else:
+        print("이미지 저장에 실패했습니다. 다시 시도해 주세요.")
